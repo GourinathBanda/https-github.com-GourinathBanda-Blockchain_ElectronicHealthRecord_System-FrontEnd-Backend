2d092fa786c6bfc668cb58ce1325f7452e666105
Tarun Kumar Singh <tarunkumarsingh98@gmail.com>
2021-02-18 18:56:48 +0530

    permission read route

--- routes/permission.js
+++ routes/permission.js
@@ -4,97 +4,124 @@ const mongoose = require("mongoose");
 var cors = require("cors");
 // var Notifications = require("../schemas/notificationSchema");
 var Permissions = require("../schemas/permissionSchema");
+var User = require("../schemas/userSchema");
 var authenticate = require("../authenticate");
 
 const permissionRouter = express.Router();
 
 permissionRouter.use(bodyParser.json());
 
-permissionRouter
-  .route("/")
-  .get(cors(), authenticate.verifyUser, (req, res, next) => {
-    Permissions.findOne({ user: req.user._id })
-      .populate("user")
-      .populate("products")
-      .exec((err, notif) => {
-        if (err) return next(err);
-        if (notif == null) {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({});
-          return;
-        }
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json(notif.products);
-      });
-  })
-
-  .post(cors(), authenticate.verifyUser, (req, res, next) => {
-    Permissions.findOne({ user: req.user._id }, (err, notif) => {
+permissionRouter.get(
+  "/readgrant/fetch:reader",
+  cors(),
+  authenticate.verifyUser,
+  (req, res, next) => {
+    // fetch reader's public key
+    User.findById(req.params.reader, (err, usr) => {
       if (err) {
-        return next(err);
+        res.sendStatus(404);
+        return;
       }
-      if (notif != null) {
-        if (notif.products.indexOf(req.body._id) !== -1) {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json(notif.products);
-        } else {
-          notif.products.push(req.body._id);
-          notif
-            .save()
-            .then((notif) => {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              res.json(notif.products);
-            })
-            .catch((err) => next(err));
-        }
-      } else {
-        Permissions.create({
-          // create new
-          user: req.user._id,
-          products: req.body._id,
-        })
-          .then(
-            (notif) => {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              res.json(notif);
-            },
-            (err) => next(err)
-          )
-          .catch((err) => next(err));
-      }
+      res.send({ usr: usr.publicKey });
+      return;
     });
-  })
+  }
+);
 
-  .put(cors(), authenticate.verifyUser, (req, res, next) => {
-    res.statusCode = 403;
-    res.send("not supported ");
-  })
+permissionRouter.post(
+  "/readgrant/send:reader",
+  cors(),
+  authenticate.verifyUser,
+  (req, res, next) => {
+    // receive location hash encrypted by reader's public key
+    // and send it to the reader
+  }
+);
+// permissionRouter
+//   .route("/")
+//   .get(cors(), authenticate.verifyUser, (req, res, next) => {
+//     Permissions.findOne({ user: req.user._id })
+//       .populate("user")
+//       .populate("products")
+//       .exec((err, notif) => {
+//         if (err) return next(err);
+//         if (notif == null) {
+//           res.statusCode = 200;
+//           res.setHeader("Content-Type", "application/json");
+//           res.json({});
+//           return;
+//         }
+//         res.statusCode = 200;
+//         res.setHeader("Content-Type", "application/json");
+//         res.json(notif.products);
+//       });
+//   })
 
-  .delete(cors(), authenticate.verifyUser, (req, res, next) => {
-    Permissions.findOne({ user: req.user._id }, (err, notif) => {
-      if (err) {
-        return next(err);
-      }
-      if (notif != null) {
-        notif.products = notif.products.filter((id) => id != req.body._id);
-        notif
-          .save()
-          .then(
-            (notif) => {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              res.json(notif.products);
-            },
-            (err) => next(err)
-          )
-          .catch((err) => next(err));
-      }
-    });
-  });
+//   .post(cors(), authenticate.verifyUser, (req, res, next) => {
+//     Permissions.findOne({ user: req.user._id }, (err, notif) => {
+//       if (err) {
+//         return next(err);
+//       }
+//       if (notif != null) {
+//         if (notif.products.indexOf(req.body._id) !== -1) {
+//           res.statusCode = 200;
+//           res.setHeader("Content-Type", "application/json");
+//           res.json(notif.products);
+//         } else {
+//           notif.products.push(req.body._id);
+//           notif
+//             .save()
+//             .then((notif) => {
+//               res.statusCode = 200;
+//               res.setHeader("Content-Type", "application/json");
+//               res.json(notif.products);
+//             })
+//             .catch((err) => next(err));
+//         }
+//       } else {
+//         Permissions.create({
+//           // create new
+//           user: req.user._id,
+//           products: req.body._id,
+//         })
+//           .then(
+//             (notif) => {
+//               res.statusCode = 200;
+//               res.setHeader("Content-Type", "application/json");
+//               res.json(notif);
+//             },
+//             (err) => next(err)
+//           )
+//           .catch((err) => next(err));
+//       }
+//     });
+//   })
+
+//   .put(cors(), authenticate.verifyUser, (req, res, next) => {
+//     res.statusCode = 403;
+//     res.send("not supported ");
+//   })
+
+//   .delete(cors(), authenticate.verifyUser, (req, res, next) => {
+//     Permissions.findOne({ user: req.user._id }, (err, notif) => {
+//       if (err) {
+//         return next(err);
+//       }
+//       if (notif != null) {
+//         notif.products = notif.products.filter((id) => id != req.body._id);
+//         notif
+//           .save()
+//           .then(
+//             (notif) => {
+//               res.statusCode = 200;
+//               res.setHeader("Content-Type", "application/json");
+//               res.json(notif.products);
+//             },
+//             (err) => next(err)
+//           )
+//           .catch((err) => next(err));
+//       }
+//     });
+//   });
 
 module.exports = permissionRouter;
